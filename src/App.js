eea5e20b0ff3a79f30116eaa2f4af426b1ee1da6
e23007 <e23007@std.it-college.ac.jp>
2024-07-31 16:26:04 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react';
+import liff from "@line/liff";
 
-function App() {
+const App=()=>{
+  const [message,setMessage]=useState("") //メッセージ
+  //メッセージ送信関数
+  const post=()=>{
+    liff.sendMessages([
+      {
+        type:'text',
+        text:`${message}`,
+      }
+    ])
+  };
+  //liffidの初期化
+  useEffect(() => {
+    const create=async ()=>{
+    console.log("start liff.init()...");
+    await liff
+      .init({ liffId: process.env.REACT_APP_LIFF_ID})
+      .then(() => {
+        console.log('init() OK')
+      })
+      .catch((error) => {
+        console.log(`liff.init() failed: ${error}`);
+      });
+    }
+    create()
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input onChange={e=>setMessage(e.target.value)} />
+      <button onClick={post}>送信</button>
+    </>
   );
-}
+};
 
 export default App;
